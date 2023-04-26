2a5449df60b7506c9d5f7483fe436fb2010287f0
25khattab <25khattab@gmail.com>
2023-04-26 05:40:23 +0200

    add audios data

--- src/components/Home/HomeData.ts
+++ src/components/Home/HomeData.ts
@@ -288,26 +288,41 @@ export class HomeData {
           imageURL: ''
         }
       ],
-      audioes: [
+      audios: [
         {
-          imagePath: '/images/home/1.jpg',
-          audioURL: ''
+          imagePath: '/images/home/media/audios/fazora-ep01-part01.jpg',
+          audioURL: 'https://soundcloud.com/user-227618101/ibzsxhniey8n'
         },
         {
-          imagePath: '/images/home/1.jpg',
-          audioURL: ''
+          imagePath: '/images/home/media/audios/fazora-ep01-part02.jpg',
+          audioURL: 'https://soundcloud.com/user-227618101/julkkywilmoz'
+        },
+
+        {
+          imagePath: '/images/home/media/audios/fazora-ep02-part01.jpg',
+          audioURL: 'https://soundcloud.com/user-227618101/lkysjkssil73'
         },
         {
-          imagePath: '/images/home/1.jpg',
-          audioURL: ''
+          imagePath: '/images/home/media/audios/fazora-ep02-part02.jpg',
+          audioURL: 'https://soundcloud.com/user-227618101/16prxkfjqout'
+        },
+
+        {
+          imagePath: '/images/home/media/audios/fazora-ep03-part01.jpg',
+          audioURL: 'https://soundcloud.com/user-227618101/0yk4w4zkgjcu'
         },
         {
-          imagePath: '/images/home/1.jpg',
-          audioURL: ''
+          imagePath: '/images/home/media/audios/fazora-ep03-part02.jpg',
+          audioURL: 'https://soundcloud.com/user-227618101/0o45gbce5cpq'
+        },
+
+        {
+          imagePath: '/images/home/media/audios/fazora-ep04-part01.jpg',
+          audioURL: 'https://soundcloud.com/user-227618101/6m3g3zasv5w5'
         },
         {
-          imagePath: '/images/home/1.jpg',
-          audioURL: ''
+          imagePath: '/images/home/media/audios/fazora-ep04-part02.jpg',
+          audioURL: 'https://soundcloud.com/user-227618101/hfiedr1ehqne'
         }
       ],
       videos: [
--- src/components/Home/Media.tsx
+++ src/components/Home/Media.tsx
@@ -7,10 +7,10 @@ import { useState } from 'react';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
-import { MdOutlineVideocam } from 'react-icons/md';
+import { AiOutlineAudio } from 'react-icons/ai';
 
 export const Media = () => {
-  const [active, setActive] = useState([0, 1, 0]);
+  const [active, setActive] = useState([1, 0, 0]);
   return (
     <div className="flex flex-col items-center ">
       <Bar title={HomeData.media.mediaTitle.ar} symbol={'movie'} color={'text-blue-500'} />
@@ -26,45 +26,108 @@ export const Media = () => {
         </AnimatedBtn>
       </div>
       <div className="flex w-full">
-        <Swiper
-          spaceBetween={10}
-          slidesPerView={5}
-          freeMode={true}
-          pagination={{
-            clickable: true
-          }}
-          modules={[FreeMode, Pagination]}
-          className="w-10/12 flex gap-6"
-        >
-          {active[0] &&
-            HomeData.media.content.audioes.map((item, index) => (
-              <SwiperSlide key={index} className=" my-14  ">
-                <div className="bg-blue-500 opacity-40"></div>
-                <img src={item.imagePath} alt="" />
-              </SwiperSlide>
-            ))}
-          {active[1] &&
-            HomeData.media.content.images.map((item, index) => (
-              <SwiperSlide key={index} className=" my-14 ">
-                <img src={item.imagePath} alt="" className="" />
-              </SwiperSlide>
-            ))}
-          {active[2] &&
-            HomeData.media.content.videos.map((item, index) => (
-              <SwiperSlide key={index} className=" my-14 ">
-                <img src={item.imagePath} alt="" />
-              </SwiperSlide>
-            ))}
-        </Swiper>
+        {active[0] ? <AudiosComponent /> : active[1] ? <VideosComponent /> : active[2] ? <ImagesComponent /> : <></>}
       </div>
     </div>
   );
 };
 
-const MediaComponent = () => {
+const AudiosComponent = () => {
   return (
-    <SwiperSlide>
-      <img src="/images/home/header-1.jpg" alt="" />
-    </SwiperSlide>
+    <Swiper
+      spaceBetween={10}
+      breakpoints={{
+        640: { slidesPerView: 2 },
+        780: { slidesPerView: 3 },
+        1024: { slidesPerView: 4 }
+      }}
+      freeMode={true}
+      pagination={{
+        clickable: true
+      }}
+      modules={[FreeMode, Pagination]}
+      className="w-10/12 flex gap-6"
+    >
+      {HomeData.media.content.audios.map((item, index) => (
+        <SwiperSlide key={index} className="my-14">
+          <div className=" relative group">
+            <img src={item.imagePath} alt="" className="" />
+            <div className="absolute top-0 bottom-0 left-0 right-0 invisible opacity-0 transition-all duration-500 group-hover:opacity-100 group-hover:visible  ">
+              <a href={item.audioURL} target="_blank">
+                <div className="  bg-blue-600 opacity-60 w-full h-full "></div>
+
+                <AiOutlineAudio className="text-white  text-4xl absolute top-1/2 right-1/2   translate-x-1/2 -translate-y-1/2" />
+              </a>
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+const ImagesComponent = () => {
+  return (
+    <Swiper
+      spaceBetween={10}
+      breakpoints={{
+        640: { slidesPerView: 2 },
+        780: { slidesPerView: 3 },
+        1024: { slidesPerView: 4 }
+      }}
+      freeMode={true}
+      pagination={{
+        clickable: true
+      }}
+      modules={[FreeMode, Pagination]}
+      className="w-10/12 flex gap-6"
+    >
+      {HomeData.media.content.audios.map((item, index) => (
+        <SwiperSlide key={index} className="my-14">
+          <div className=" relative group">
+            <img src={item.imagePath} alt="" className="" />
+            <div className="absolute top-0 bottom-0 left-0 right-0 invisible opacity-0 transition-all duration-500 group-hover:opacity-100 group-hover:visible  ">
+              <a href={item.audioURL} target="_blank">
+                <div className="  bg-blue-600 opacity-60 w-full h-full "></div>
+
+                <AiOutlineAudio className="text-white  text-4xl absolute top-1/2 right-1/2   translate-x-1/2 -translate-y-1/2" />
+              </a>
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+const VideosComponent = () => {
+  return (
+    <Swiper
+      spaceBetween={10}
+      breakpoints={{
+        640: { slidesPerView: 2 },
+        780: { slidesPerView: 3 },
+        1024: { slidesPerView: 4 }
+      }}
+      freeMode={true}
+      pagination={{
+        clickable: true
+      }}
+      modules={[FreeMode, Pagination]}
+      className="w-10/12 flex gap-6"
+    >
+      {HomeData.media.content.audios.map((item, index) => (
+        <SwiperSlide key={index} className="my-14">
+          <div className=" relative group">
+            <img src={item.imagePath} alt="" className="" />
+            <div className="absolute top-0 bottom-0 left-0 right-0 invisible opacity-0 transition-all duration-500 group-hover:opacity-100 group-hover:visible  ">
+              <a href={item.audioURL} target="_blank">
+                <div className="  bg-blue-600 opacity-60 w-full h-full "></div>
+
+                <AiOutlineAudio className="text-white  text-4xl absolute top-1/2 right-1/2   translate-x-1/2 -translate-y-1/2" />
+              </a>
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
